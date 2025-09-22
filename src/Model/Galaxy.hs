73809e28e76616f979839abe87d0d09ccb9f8792
brunobock-del <bruno.bock@irriga.net>
2025-09-22 21:03:39 +0000

    CRUD galáxias

--- src/Model/Galaxy.hs
+++ src/Model/Galaxy.hs
@@ -1,44 +1,81 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
-import Database.SQLite.Simple
-import Database.SQLite.Simple.FromRow
-import Data.Aeson (FromJSON, ToJSON)
-import Data.Text (Text)
+module Model.Galaxy
+  ( Galaxy(..)
+  , GalaxyInsert(..)
+  , initGalaxyDB
+  , insertGalaxy
+  , getGalaxies
+  , getGalaxyById
+  , updateGalaxy
+  , deleteGalaxy
+  ) where
 
+import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow ()
+import Data.Text (Text)
+import GHC.Generics (Generic)
+
+-- Definição do modelo
 data Galaxy = Galaxy
-    { id :: Int
-    , name :: Text
-    , type :: Text
-    , note :: Maybe Text
+    { galaxyId   :: Int
+    , galaxyName :: Text
+    , galaxyType :: Text
+    , galaxyNote :: Maybe Text
     } deriving (Show, Generic)
-    
-instance FromRow where 
+
+-- Tipo para inserir no banco (sem ID)
+data GalaxyInsert = GalaxyInsert
+    { giName :: Text
+    , giType :: Text
+    , giNote :: Maybe Text
+    } deriving (Show, Generic)
+
+instance FromRow Galaxy where 
     fromRow = Galaxy <$> field <*> field <*> field <*> field
 
-instance ToRow where
-    toRow (Galaxy _ name type note) = toRow(name, type, note)
+instance ToRow GalaxyInsert where
+    toRow (GalaxyInsert name gtype note) = toRow (name, gtype, note)
 
+-- Criação da tabela
 initGalaxyDB :: Connection -> IO ()
-initGalaxyDB conn = execute_ conn "CREATE TABLE IF NOT EXISTS galaxies (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, type TEXT NOT NULL, note TEXT NULL)"
+initGalaxyDB conn =
+    execute_ conn
+      "CREATE TABLE IF NOT EXISTS galaxies \
+      \(id INTEGER PRIMARY KEY AUTOINCREMENT, \
+      \name TEXT NOT NULL, \
+      \galaxyType TEXT NOT NULL, \
+      \note TEXT NULL)"
 
-insertGalaxy :: Connection -> Galaxy -> IO ()
-insertGalaxy conn galaxy = execute conn "INSERT INTO galaxies (name, type, note) VALUES (?, ?, ?)" (toRow galaxy)
+-- Insert
+insertGalaxy :: Connection -> GalaxyInsert -> IO ()
+insertGalaxy conn g =
+    execute conn
+      "INSERT INTO galaxies (name, galaxyType, note) VALUES (?, ?, ?)"
+      g
 
+-- Select *
 getGalaxies :: Connection -> IO [Galaxy]
 getGalaxies conn = query_ conn "SELECT * FROM galaxies"
 
+-- Select by id
 getGalaxyById :: Connection -> Int -> IO (Maybe Galaxy)
-getGalaxyById conn id = do 
-    result <- query "SELECT * FROM galaxies WHERE id = ?" (Only id)
+getGalaxyById conn gid = do 
+    result <- query conn
+        "SELECT * FROM galaxies WHERE id = ?" (Only gid)
     return $ case result of
         [galaxy] -> Just galaxy
         _        -> Nothing
 
-updateGalaxy :: Connection -> Int -> Galaxy -> IO ()
-updateGalaxy conn id galaxy = 
-    execute conn "UPDATE galaxies SET name = ?, type = ?, note = ? WHERE id = ?" (name galaxy, type galaxy, note galaxy, id)
+-- Update
+updateGalaxy :: Connection -> Int -> GalaxyInsert -> IO ()
+updateGalaxy conn gid g = 
+    execute conn
+      "UPDATE galaxies SET name = ?, galaxyType = ?, note = ? WHERE id = ?"
+      (giName g, giType g, giNote g, gid)
 
+-- Delete
 deleteGalaxy :: Connection -> Int -> IO ()
-deleteGalaxy conn id =
-    execute conn "DELETE FROM galaxies WHERE id = ?" (Only id)
+deleteGalaxy conn gid =
+    execute conn "DELETE FROM galaxies WHERE id = ?" (Only gid)
---
+++ src/View/GalaxyView.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module View.GalaxyView
+  ( galaxyToJSON
+  ) where
+
+import Model.Galaxy
+import Data.Aeson (ToJSON, toJSON, object, (.=))
+import Data.Text.Lazy (Text)
+
+-- Instância ToJSON customizada
+instance ToJSON Galaxy where
+  toJSON (Galaxy gid name gtype note) =
+    object
+      [ "id"   .= gid
+      , "name" .= name
+      , "type" .= gtype
+      , "note" .= note
+      ]
+
+-- função auxiliar, se quiser formatar ou manipular antes de enviar
+galaxyToJSON :: Galaxy -> Galaxy
+galaxyToJSON = id  -- aqui você poderia limpar ou processar campos
